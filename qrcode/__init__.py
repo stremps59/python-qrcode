35c549524c7e1991543c599c4ce02ce2179d420c
stremps59 <stremps@sbcglobal.net>
2025-06-06 15:54:15 -0700

    Update __init__.py

--- qrcode/__init__.py
+++ qrcode/__init__.py
@@ -1,11 +1,10 @@
-from qrcode_styled.main import make, Mode
+from qrcode_styled.main import QRCode
 from qrcode_styled.constants import (
     ERROR_CORRECT_L,
     ERROR_CORRECT_M,
     ERROR_CORRECT_Q,
     ERROR_CORRECT_H,
 )
-
 
 from   # noqaqrcode_styled.image import Image
 
